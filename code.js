4577c7ad57732175abd787d1a08f27a30e0e980f
Hannah Ripley <hannah@Hannahs-MacBook-Pro.local>
2019-07-15 11:15:03 -0600

    made the zones work, as far as i can tell

--- code.js
+++ code.js
@@ -12,7 +12,9 @@ var app= new Vue ({
       color: "",
 
         educationlist:[],
-        workexplist:[],
+        workexplist:[            
+          // position: 0, // add to database
+        ],
         accomplishmentlist: [],
         extracurricularlist:[],
         languageslist:[],
@@ -48,8 +50,6 @@ var app= new Vue ({
             description: "",
             start_menu: false,
             end_menu: false,
-            position: 0, // for each v-card in a template, do for loop through all categories and see which has the desired position
-
         },
         educationEdit: {
           college: "",
@@ -138,7 +138,16 @@ var app= new Vue ({
 
       add_remove: "",
 
-      workexpposition: "",
+      statementposition: 0,
+      workexpposition: 0,
+      educationposition: 0,
+      accomplishmentposition: 0,
+      extracurricularposition: 0,
+      languagesposition: 0,
+      programsposition: 0,
+      softskillsposition: 0,
+      awardsposition: 0,
+
       position1: {},
 
       zone1: [],
@@ -305,39 +314,256 @@ var app= new Vue ({
       },
 
 
-      priorityOne: function (exp) {
-        this.priority1.push(exp);
+      apply: function (type) {
+        var display = [];
+        if (type == "workexp") {
+          display = this.workexpdisplay;
+          if (this.workexpposition == "1") {
+            this.apply1(type,display,);
+          }
+          else if (this.workexpposition == "2") {
+            this.apply2(type,display);
+          }
+          else if (this.workexpposition == "3") {
+            this.apply3(type,display);
+          }
+          else if (this.workexpposition == "4") {
+            this.apply4(type,display);
+          }
+          else if (this.workexpposition == "5") {
+            this.apply5(type,display);
+          }
+          else if (this.workexpposition == "6") {
+            this.apply6(type,display);
+          }
+          else if (this.workexpposition == "7") {
+            this.apply7(type,display);
+          }
+        }
+        else if (type == "education") {
+          display = this.educationdisplay;
+          if (this.educationposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.educationposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.educationposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.educationposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.educationposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.educationposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.educationposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "statement") {
+          display = this.statementdisplay;
+          if (this.statementposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.statementposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.statementposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.statementposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.statementposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.statementposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.statementposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "accomplishment") {
+          display = this.accomplishmentdisplay;
+          if (this.accomplishmentposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.accomplishmentposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.accomplishmentposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.accomplishmentposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.accomplishmentposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.accomplishmentposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.accomplishmentposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "award") {
+          display = this.awardsdisplay;
+          if (this.awardsposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.awardsposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.awardsposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.awardsposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.awardsposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.awardsposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.awardsposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "language") {
+          display = this.languagesdisplay;
+          if (this.languagesposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.languagesposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.languagesposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.languagesposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.languagesposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.languagesposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.languagesposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "extracurricular") {
+          display = this.extracurricularndisplay;
+          if (this.extracurricularnposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.extracurricularnposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.extracurricularnposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.extracurricularnposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.extracurricularnposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.extracurricularnposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.extracurricularnposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "programs") {
+          display = this.programsdisplay;
+          if (this.programsposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.programsposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.programsposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.programsposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.programsposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.programsposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.programsposition == "7") {
+            this.apply7(type, display);
+          }
+        }
+        else if (type == "skills") {
+          display = this.softskillsdisplay;
+          if (this.softskillsposition == "1") {
+            this.apply1(type, display);
+          }
+          else if (this.softskillsposition == "2") {
+            this.apply2(type, display);
+          }
+          else if (this.softskillsposition == "3") {
+            this.apply3(type, display);
+          }
+          else if (this.softskillsposition == "4") {
+            this.apply4(type, display);
+          }
+          else if (this.softskillsposition == "5") {
+            this.apply5(type, display);
+          }
+          else if (this.softskillsposition == "6") {
+            this.apply6(type, display);
+          }
+          else if (this.softskillsposition == "7") {
+            this.apply7(type, display);
+          }
+        }
       },
 
 
-      apply1: function (type) {
-        this.zone1 = this.workexpdisplay;
-        this.zone1_type = type;
+      apply1: function (type, display) {
+          this.zone1 = display;
+          this.zone1_type = type;
       },
-      apply2: function (type) {
-        this.zone2 = this.workexpdisplay;
+      apply2: function (type, display) {
+        this.zone2 = display;
         this.zone2_type = type;
       },
-      apply3: function (type) {
-        this.zone3 = this.workexpdisplay;
-        this.zone3_type = type;
+      apply3: function (type, display) {
+          this.zone3 = display;
+          this.zone3_type = type;
       },
-      apply4: function (type) {
-        this.zone4 = this.workexpdisplay;
-        this.zone4_type = type;
+      apply4: function (type, display) {
+          this.zone4 = display;
+          this.zone4_type = type;
       },
-      apply5: function (type) {
-        this.zone5 = this.workexpdisplay;
-        this.zone1_type = type;
+      apply5: function (type, display) {
+          this.zone5 = display;
+          this.zone5_type = type;
       },
-      apply6: function (type) {
-        this.zone6 = this.workexpdisplay;
-        this.zone6_type = type;
+      apply6: function (type, display) {
+          this.zone6 = display;
+          this.zone6_type = type;
       },
-      apply7: function (type) {
-        this.zone7 = this.workexpdisplay;
-        this.zone7_type = type;
+      apply7: function (type, display) {
+          this.zone7 = display;
+          this.zone7_type = type;
       },
+      
 
       newKellyColorPickerMain: function () { 
         if (this.pickingColorMain == false) {
